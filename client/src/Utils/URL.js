d136e88e310213dd6b4ca69e0e040ed9b1415ce6
glossada <gabriellossada@gmail.com>
2023-08-15 20:05:59 -0300

    rutas externas

--- client/src/Utils/URL.js
+++ client/src/Utils/URL.js
@@ -1,4 +1,4 @@
-const RECIPES = 'http://localhost:3001/recipes/';
-const DIETS = 'http://localhost:3001/diets/';
+const RECIPES = 'https://food-back-ewuh.onrender.com/recipes/';
+const DIETS = 'https://food-back-ewuh.onrender.com/diets/';
 const ITEMS_PER_PAGE=9;
 export { RECIPES, DIETS,ITEMS_PER_PAGE };
